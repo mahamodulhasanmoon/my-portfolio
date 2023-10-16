09e71daa88f41b54135292cc61fd41698692c75a
Mahamodul Hasan Moon <mahamodulhasan.moon@gmail.com>
2023-10-17 02:29:13 +0600

    done

---
+++ src/components/Particles.tsx
@@ -0,0 +1,100 @@
+"use client"
+import { useCallback } from "react";
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
+import { loadSlim } from "tsparticles-slim";
+
+const Particle = () => {
+  const particlesInit = useCallback(async (engine:any) => {
+    console.log(engine);
+    //await loadFull(engine);
+    await loadSlim(engine);
+  }, []);
+
+  const particlesLoaded = useCallback(async (container:any) => {
+    await console.log(container);
+  }, []);
+
+  return (
+    <Particles
+    id="tsparticles"
+    init={particlesInit}
+    loaded={particlesLoaded}
+    options={{
+      background: {
+        color: {
+          value: "#0A192F", // Set a dark background color
+        },
+      },
+      fpsLimit: 120,
+      interactivity: {
+        events: {
+          onClick: {
+            enable: true,
+            mode: "push",
+          },
+          onHover: {
+            enable: true,
+            mode: "repulse",
+          },
+          resize: true,
+        },
+        modes: {
+          push: {
+            quantity: 18,
+          },
+          repulse: {
+            distance: 150,
+            duration: 0.6,
+          },
+        },
+      },
+      particles: {
+        color: {
+          value: "#00BFFF", 
+        },
+        links: {
+          color: "#7F00FF",
+          distance: 150,
+          enable: true,
+          opacity: 0.3,
+          width: 5,
+        },
+        move: {
+          direction: "none",
+          enable: true,
+          outModes: {
+            default: "bounce",
+          },
+          random: false,
+          speed: 2,
+          straight: false,
+        },
+        number: {
+          density: {
+            enable: true,
+            area: 800,
+          },
+          value: 50,
+        },
+        opacity: {
+          value: 0.3,
+        },
+        shape: {
+          type: "star",
+          options: {
+            polygon: {
+              nb_sides: 5, // Number of sides
+            },
+          },
+        },
+        size: {
+          value: { min: 1, max: 5 },
+        },
+      },
+      detectRetina: true,
+    }}
+  />
+  );
+};
+export default Particle;
--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,4 +1,5 @@
 import Email from "@/components/Email";
+import Particle from "@/components/Particles";
 import SocialIcons from "@/components/SocialIcons";
 import Navbar from "@/sections/Navbar";
 
@@ -6,6 +7,7 @@ import Navbar from "@/sections/Navbar";
 export default function MainLayout() {
   return (
     <>
+    <Particle/>
     <Navbar/>
     <Email/>
     <SocialIcons/>
